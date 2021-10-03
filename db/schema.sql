4f9c83fe8a2240deba2911b4bac4ade6d3e83e59
stephaniecjohansson <7steph7@gmail.com>
2021-10-03 15:00:30 -0600

    database

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,9 @@ CREATE DATABASE Employee_db
 
 CREATE TABLE department( 
     id INT PRIMARY KEY, 
-    name VARCHAR(30) );
+    name VARCHAR(30),
+    PRIMARY KEY (dept_id)
+);
 
 
 CREATE TABLE employee( 
@@ -12,10 +14,17 @@ CREATE TABLE employee(
     first_name VARCHAR(30), 
     last_name VARCHAR(30), 
     role_id INT, 
-    manager_id INT );
+    manager_id INT,
+    PRIMARY KEY (emp_id),
+    FOREIGN KEY (emp_role_id) REFERENCES company_role(role_id),
+    FOREIGN KEY (manager_id) REFERENCES employees(emp_id)
+);
 
-CREATE TABLE role( 
-    id INT PRIMARY KEY, 
+CREATE TABLE company_role( 
+    role_id INT, 
     title VARCHAR(40), 
     salary DECIMAL, 
-    department_id INT );
+    department_id INT,
+    PRIMARY KEY (role_id),
+    FOREIGN KEY (dept_id) REFERENCES department(dept_id)
+);
